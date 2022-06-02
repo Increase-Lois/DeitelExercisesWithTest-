5b0f75152a495e98b7fac07b3be2c6c3867febff
increase <increasebruce@gmail.com>
2022-06-02 20:18:48 +0100

    ChapterThreeTest

---
+++ test/ChapterThree/HeartRateCalcTest.java
@@ -0,0 +1,10 @@
+package ChapterThree;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class HeartRateCalcTest {
+    @BeforeEach
+    void setUp(){
+        HeartRateCalc rateCalc = new HeartRateCalc();
+    }
+}
