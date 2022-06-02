05afce20d76ca1d38c8cb039b5f3889a6a71845c
increase <increasebruce@gmail.com>
2022-06-02 20:11:46 +0100

    ChapterTwoTest

---
+++ test/ChapterTwo/GreatPyramidTest.java
@@ -0,0 +1,23 @@
+package ChapterTwo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+/*
+(Statistics for the Great Pyramid of Giza) The Great Pyramid of Giza is considered an en-
+gineering marvel of its time. Use the web to get statistics related to the Great Pyramid of Giza, and
+find the estimated number of stones used to build it, the average weight of each stone, and the num-
+ber of years it took to build. Create an application that calculates an estimate of how much, by
+weight, of the pyramid was built each year, each hour, and each minute as it was being built. The
+application should input the following information:
+a) Estimated number of stones used.
+b) Average weight of each stone.
+c) Number of years taken to build the pyramid (assuming a year comprises 365 days).
+ */
+public class GreatPyramidTest {
+//    @Test
+//    public void obtainEstimatedStatisticsOfthePyramidTest(){
+//        assertEquals(22.97,GreatPyramid.getEstimatedAverageStoneWeight());
+//    }
+
+}
